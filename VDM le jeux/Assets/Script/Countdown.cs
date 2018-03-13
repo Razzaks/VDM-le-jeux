5b2e78bb8d4eb21a48cf34adfc884cbf42fcf18d
Unknown <layecwilliam@gmail.com>
2018-03-13 15:18:12 +0100

    Squelette code switch game

---
+++ VDM le jeux/Assets/Script/Countdown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public float time = 10f;
+    public GameManager GameManager;
+    bool CountDownOn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(timer());
+        time += 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    IEnumerator timer()
+    {
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            time--;
+            GetComponent<Text>().text = time.ToString();
+
+        }
+        if(time == 0)
+        {
+            GameManager.NextGame();
+        }
+    }
+}
