346cb60770e28da94d01c7a2d4e01de2d94edb3d
adrien rochard <adrienrochard@gmail.com>
2018-03-13 15:20:46 +0100

    menu-test1
    prof-test

---
+++ VDM le jeux/Assets/Script/portablescript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portablescript : MonoBehaviour {
+    public Object portable;
+
+
+    void Start () {
+        transform.position = new Vector3(165, -20,-60);       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        transform.position = new Vector3(165, 20, -60);
+
+        if (Input.GetKey("s"))
+        transform.position = new Vector3(165, -20, -60);
+
+    }
+
+    }
